77f8c17f3834f9a6534ed5be1b5f564e2505e919
pxdalza <pxdalza@cibertec.edu.pe>
2020-06-16 21:03:55 -0500

    guia de git en el proyecto consola

--- Console/Program.cs
+++ Console/Program.cs
@@ -10,31 +10,31 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            Model context = new Model();
+            //Model context = new Model();
 
-            //listar
-            var users = context.Users.ToList(); // equivalente a un selct *
+            ////listar
+            //var users = context.Users.ToList(); // equivalente a un selct *
        
 
-            var users1 = context.Users.Select(x => new Users { IdUser = x.IdUser }).ToList();
+            //var users1 = context.Users.Select(x => new Users { IdUser = x.IdUser }).ToList();
 
-            //obtener
-            var user = context.Users.Where(x => x.IdUser.Equals(new Guid())).Single(); //afirmas de que el objeto existe y no sera null
-            var user2 = context.Users.Where(x => x.IdUser.Equals(new Guid())).SingleOrDefault(); // no estas seguro de q existe por lo que si no esta el objeto de respuesta es null.
-
-
-            //crear
-            var u = new Users();
-            u.IdUser = new Guid();
+            ////obtener
+            //var user = context.Users.Where(x => x.IdUser.Equals(new Guid())).Single(); //afirmas de que el objeto existe y no sera null
+            //var user2 = context.Users.Where(x => x.IdUser.Equals(new Guid())).SingleOrDefault(); // no estas seguro de q existe por lo que si no esta el objeto de respuesta es null.
 
 
-            //update
-            user.Name = "ssssss";
+            ////crear
+            //var u = new Users();
+            //u.IdUser = new Guid();
 
-            // delete
-            context.Users.Remove(user);
 
-            context.SaveChanges();
+            ////update
+            //user.Name = "ssssss";
+
+            //// delete
+            //context.Users.Remove(user);
+
+            //context.SaveChanges();
 
         }
     }
